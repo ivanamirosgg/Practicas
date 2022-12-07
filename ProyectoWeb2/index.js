364c33c0582ce61f83443fd024c7dc988bfa33a6
ivanamirosgg <102983934+ivanamirosgg@users.noreply.github.com>
2022-12-07 14:32:22 -0600

    Add files via upload
    ProyectoWeb

---
+++ ProyectoWeb2/index.js
@@ -0,0 +1,22 @@
+function cargaMenu(){
+    fetch('menu/index.html')
+    .then(response=> response.text())
+    .then(text=> document.getElementById('menu').innerHTML = text);
+}
+
+function cargaInicio(){
+    fetch('inicio/index.html')
+    .then(response=> response.text())
+    .then(text=> document.getElementById('inicio').innerHTML = text);
+}
+
+function cargaPie(){
+    fetch('piedepagina/index.html')
+    .then(response=> response.text())
+    .then(text=> document.getElementById('piedepagina').innerHTML = text);
+}
+
+cargaMenu();
+cargaInicio();
+cargaPie();
+
